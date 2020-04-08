18bc80cc3c5e97af42704ddf29daa527a9d3cd07
Cindy Jacob <jacobcindy@live.be>
2020-04-08 14:33:28 +0200

    add gem i18n locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "homepage/index"
-  get "homepage/about"
+
   root "homepage#index"
+  get '/:locale' => 'homepage#index'
+
+  scope "(:locale)", locale: /fr|en|nl|ar/ do
+    get 'about' => "homepage#about"
+    resources :homepage , only: [:index, :about]
+  end
 end
+  
