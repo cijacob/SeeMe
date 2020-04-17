092dbe34e585b5fb823de1d4456b258318fcde39
Cindy Jacob <jacobcindy@live.be>
2020-04-17 21:41:56 +0200

    try to fix heroku problem with import bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,8 +16,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import "bootstrap"; 
-import "../stylesheets/application";
+import "bootstrap"
+import "../stylesheets/application"
 
 
 
