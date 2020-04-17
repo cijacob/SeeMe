91cad10bfd3be15c56d5a75624d1e0166e61b0bc
Cindy Jacob <jacobcindy@live.be>
2020-04-17 21:50:47 +0200

    try to fix problem font-awesome heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import "bootstrap"
+import "font-awesome"
 import "../stylesheets/application"
 
 
