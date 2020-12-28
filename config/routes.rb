d03a815a6439989cdeeacbebb1d4412d932906af
Cindy Jacob <jacobcindy@live.be>
2020-12-28 17:19:53 +0100

    clean files and work on student vue

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'students/index'
-  get 'students/new'
   get 'students/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
