e72ce44c32f1e750d7202f92812b6823aff9fb09
Cindy Jacob <jacobcindy@live.be>
2021-01-05 22:24:47 +0100

    work on page support us

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,15 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  const link = document.querySelector('.title_link'); 
+  const content = document.querySelector('.section')
+
+  link.addEventListener('click', function(){
+    console.log('coucou')
+    content.classList.toggle('active')
+  })
+
+});
+
