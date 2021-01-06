24898aa837fbed0dcaf48bb473cff9da94830fee
Cindy Jacob <jacobcindy@live.be>
2021-01-06 15:19:39 +0100

    page help us

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -5,12 +5,26 @@ const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  const link = document.querySelector('.title_link'); 
-  const content = document.querySelector('.section')
 
-  link.addEventListener('click', function(){
-    console.log('coucou')
-    content.classList.toggle('active')
+
+
+  const links = document.querySelectorAll('.link_tab_container'); 
+  const linksContainer = document.querySelector('.tab_container'); 
+  const contents = document.querySelectorAll('.section__content'); 
+  
+
+  linksContainer.addEventListener('click', function(e){
+    clicked = e.target.closest('.link_tab_container');
+    
+    if(!clicked) return
+    links.forEach( l => l.classList.remove('active'))
+    contents.forEach( c => c.classList.remove('section_content__active'));
+
+    clicked.classList.add('active')
+
+    document
+    .querySelector(`.section__content${clicked.dataset.tab}`)
+    .classList.add('section_content__active');
   })
 
 });
