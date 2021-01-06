acde8cd0da49402d4e92d45b867ccd8fb811a19c
Cindy Jacob <jacobcindy@live.be>
2021-01-06 16:47:43 +0100

    create model subsection

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -7,7 +7,6 @@ channels.keys().forEach(channels)
 document.addEventListener("DOMContentLoaded", function(event) {
 
 
-
   const links = document.querySelectorAll('.link_tab_container'); 
   const linksContainer = document.querySelector('.tab_container'); 
   const contents = document.querySelectorAll('.section__content'); 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_100313) do
+ActiveRecord::Schema.define(version: 2021_01_06_154600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,12 @@ ActiveRecord::Schema.define(version: 2021_01_03_100313) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "project_id"
     t.index ["project_id"], name: "index_students_on_project_id"
+  end
+
+  create_table "subsections", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "trainees", force: :cascade do |t|
