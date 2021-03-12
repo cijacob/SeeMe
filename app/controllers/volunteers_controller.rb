2f351476c971fcb376caf0f71b1ae9748a796833
Cindy Jacob <jacobcindy@live.be>
2021-03-12 22:38:41 +0100

    add volunteer work on new design and text

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,7 +1,14 @@
 class VolunteersController < ApplicationController
   def new
+    
   end
 
   def create
   end
+
+  private 
+
+  def vonlunteer_params
+    params.require(:volunteers).permit(:id, :section_id, :subsection_id)
+  end 
 end
